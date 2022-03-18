d9129b9bb63029636457df4b6a4ef8f59a4f4776
jack-app <lico.tomato69@gmail.com>
2022-03-19 03:59:21 +0900

    トップページを作った

--- front/components/generalButton.tsx
+++ front/components/generalButton.tsx
@@ -1,8 +1,22 @@
-type GeneralButton = {
-  readonly text: string;
-};
-const GeneralButton = (generalButton: GeneralButton) => {
-  return <button className="p-2">{generalButton.text}</button>;
-};
+import Link from 'next/link'
+import router from 'next/router'
+import Questions from '../pages/question'
 
-export default GeneralButton;
+type GeneralButton = {
+  readonly text: string
+  readonly content: 'question' | 'register'
+}
+const GeneralButton = (generalButton: GeneralButton) => {
+  const path = `../${generalButton.content}`
+  return (
+    <div>
+      <Link href={path}>
+        <div className="m-5 rounded bg-orange-500 p-3 pr-4 pl-4 text-white hover:bg-orange-400">
+          {generalButton.text}
+        </div>
+      </Link>
+    </div>
+  )
+}
+
+export default GeneralButton
--- front/pages/index.tsx
+++ front/pages/index.tsx
@@ -9,28 +9,44 @@ const Home: NextPage = () => {
   return (
     <>
       <Head>
-        <title>Create Next App</title>
+        <title>第二外国語アプリ</title>
         <link rel="icon" href="/favicon.ico" />
       </Head>
       <Layout header="Next.js" title="第二外国語アプリ">
-        <div className="">
-          <div>
-            <p className="text-5xl text-orange-800">Bonjour le monde!!</p>
-            <p className="text-xl text-orange-800">
-              人称や格を楽しく学べるアプリ
-            </p>
+        <div className="flex justify-center bg-yellow-100">
+          <div className="flex w-full items-end justify-between p-7 md:w-[90%] lg:w-[85%]">
+            <div className="ml-5 text-orange-800">
+              <p className="p-3 text-[1.75rem] sm:text-5xl md:text-5xl lg:text-7xl">
+                Bonjour le monde!!
+              </p>
+              <p className="p-3 pt-4 text-[0.5rem] sm:text-[1rem] md:text-xl lg:text-3xl">
+                人称や格を楽しく学べるアプリ
+              </p>
+            </div>
+            <img
+              className="mr-5 w-[50%] p-1 sm:w-[40%] lg:w-[30%]"
+              src="../amico.png"
+            />
           </div>
-          <img src="../amico.png" />
         </div>
-        <title>Create Next App</title>
-        <link rel="icon" href="/favicon.ico" />
-        <div className="">
-          <div className="">
-            <Link href="/register">問題を解く</Link>
-            <br />
-            <Link href="/question">問題を登録する</Link>
-            <GeneralButton text="フランス語" />
-            <GeneralButton text="ロシア語" />
+        <div className="m-5 text-center">
+          <div className="pt-2 pb-2 lg:pt-4">
+            <p className="p-3 text-[1.75rem] text-orange-900 sm:text-2xl md:text-3xl lg:text-4xl">
+              問題を解く
+            </p>
+            <div className="flex justify-center">
+              <GeneralButton text="フランス語" content="question" />
+              <GeneralButton text="ロシア語" content="question" />
+            </div>
+          </div>
+          <div className="pt-2 pb-2 lg:pt-4">
+            <p className="p-3 text-[1.75rem] text-orange-900 sm:text-2xl md:text-3xl lg:text-4xl">
+              問題を登録する
+            </p>
+            <div className="flex justify-center">
+              <GeneralButton text="フランス語" content="register" />
+              <GeneralButton text="ロシア語" content="register" />
+            </div>
           </div>
         </div>
       </Layout>
