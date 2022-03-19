875f5540d9ba2d35c39de801216a5ffbd89fe9b8
nac-39 <83295760+nac-39@users.noreply.github.com>
2022-03-19 16:27:05 +0900

    ADD/backend/フランス語の性のい 問題を返すAPI

--- backend/api/functions/ru.py
+++ backend/api/functions/ru.py
@@ -13,7 +13,7 @@ gender = {
 }
 
 
-def noun_gender(words: List[str])-> Schema_noun_gender:
+def noun_gender(words: List[str]) -> Schema_noun_gender:
     morph = pymorphy2.MorphAnalyzer()
     q_and_a = list()
     for w in words:
@@ -23,39 +23,41 @@ def noun_gender(words: List[str])-> Schema_noun_gender:
         for p in w_parsed:
             # 名詞かつ性別のタグがある場合
             if ("NOUN" in p.tag) and (p.tag.gender):
-                normal_form =  p.normal_form if p.normal_form else w
-                q_and_a.append(QAO(q=[normal_form], a=gender[p.tag.gender], ordinal=w))
+                normal_form = p.normal_form if p.normal_form else w
+                q_and_a.append(
+                    QAO(q=[normal_form], a=gender[p.tag.gender], ordinal=w))
                 break
     return Schema_noun_gender(q_and_a=q_and_a)
 
 # 問題のデータをgen_schemeに合わせた形に整形する
-def format_gen_scheme(func: Schema_noun_gender)-> List[gen_schema]:
+
+
+def format_gen_scheme(func: Schema_noun_gender) -> List[gen_schema]:
     meta = {
         "language": "ru",
         "questionType": "choices",
     }
-    i = 0 # id用
-    qa_list=list()
+    i = 0  # id用
+    qa_list = list()
     q_schema = gen_schema.Question
     for qa in func.q_and_a:
         # 答えになりうる集合から正解を取り除く
-        wrong_choices = [w for w in gender.values()if w != qa.a] #set(func.choices) - set([qa.a])
+        # set(func.choices) - set([qa.a])
+        wrong_choices = [w for w in gender.values()if w != qa.a]
         # gen_schemaを作る
         gen = gen_schema(meta=meta,
-                    question = q_schema(
-                            id=i,
-                            q_sentence=func.q_sentence,
-                            q_variable=qa.q,
-                            choices={
-                                "correct": qa.a, # 正解
-                                "wrong": wrong_choices
-                            },
-                            ordinal_word=qa.ordinal
-                    )
-        )
-        
-        i+=1
+                         question=q_schema(
+                             id=i,
+                             q_sentence=func.q_sentence,
+                             q_variable=qa.q,
+                             choices={
+                                 "correct": qa.a,  # 正解
+                                 "wrong": wrong_choices
+                             },
+                             ordinal_word=qa.ordinal
+                         )
+                         )
+
+        i += 1
         qa_list.append(gen)
     return qa_list
-
-
--- backend/api/routers/generate.py
+++ backend/api/routers/generate.py
@@ -4,14 +4,15 @@ from typing import List
 from api.schemas.generate import Generate as gen_schema
 from api.schemas.request import Request as rq_schema
 from api.functions.ru import noun_gender, format_gen_scheme
+from api.functions.fr import fr_noun_gender, gen_schema_fr
 router = APIRouter()
 
 @router.post("/api/generate", response_model=List[gen_schema])
 async def generate_questions(rq: rq_schema)-> gen_schema:
     if rq.language == "ru":
-        if rq.type == "choice":
+        if rq.type == "noun_gender_choice":
             return format_gen_scheme(noun_gender(rq.words))
     
     elif rq.language == "fr":
-        if rq.type == "":
-            pass
+        if rq.type == "noun_gender_choice":
+            return gen_schema_fr(fr_noun_gender(rq.words))
